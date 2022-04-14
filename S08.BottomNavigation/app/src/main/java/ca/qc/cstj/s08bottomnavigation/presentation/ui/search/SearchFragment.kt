8e8eed13354afab69f321c4a0ec97ad4453258a4
lemben01 <1868282@cstj.qc.ca>
2022-04-14 16:43:41 -0400

    map

--- S08.BottomNavigation/app/src/main/java/ca/qc/cstj/s08bottomnavigation/presentation/ui/search/SearchFragment.kt
+++ S08.BottomNavigation/app/src/main/java/ca/qc/cstj/s08bottomnavigation/presentation/ui/search/SearchFragment.kt
@@ -10,6 +10,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import ca.qc.cstj.s08bottomnavigation.R
 import ca.qc.cstj.s08bottomnavigation.core.Constants
 import ca.qc.cstj.s08bottomnavigation.core.LoadingResource
@@ -17,6 +18,7 @@ import ca.qc.cstj.s08bottomnavigation.core.text
 import ca.qc.cstj.s08bottomnavigation.databinding.FragmentSearchBinding
 import ca.qc.cstj.s08bottomnavigation.domain.models.Meteo
 import com.bumptech.glide.Glide
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.snackbar.Snackbar
 import java.time.Instant
 import java.time.ZoneId
@@ -32,6 +34,8 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     private val viewModel: SearchViewModel by viewModels()
 
     private lateinit var ctlMainActivity : ConstraintLayout
+
+    private var position: LatLng? = null
 
     //Dans l'activite la majorite de code est dans onCreate() => onViewCreated()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -67,10 +71,21 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         binding.btnSearch.setOnClickListener {
             viewModel.search(binding.tilSearch.text)
         }
+
+        binding.fabLocation.setOnClickListener {
+            val action = SearchFragmentDirections
+                .actionNavigationSearchToMapsActivity(position!!)
+            findNavController().navigate(action)
+        }
     }
+
+
 
     private fun showMeteo(meteo: Meteo) {
        //TODO: Affichage de chacunes des informations de la meteo
+
+        position = LatLng(meteo.latitude, meteo. longitude)
+
         with(binding){
             txvCity.text = meteo.city
             txvTemperature.text = getString(R.string.temperatureFormat, meteo.temperature)
@@ -85,6 +100,7 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
                 val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
                // Instant.ofEpochSecond(meteo.timestamp.toLong()).atZone(ZoneOffset.UTC).plusSeconds(7200)
+
 
                 txvDateAPI.text = formatter.format(Instant.ofEpochSecond(meteo.timestamp.toLong()).atZone(ZoneOffset.UTC)
                     .plusSeconds(meteo.timezone.toLong()))
